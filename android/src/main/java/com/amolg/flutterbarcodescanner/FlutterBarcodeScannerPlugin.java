4c4cbdb602316bff8f9b5876382afb68cde6481d
MP <peczwy@gmail.com>
2020-09-02 15:04:11 +0200

    Synchronized OnActivityResult

--- android/src/main/java/com/amolg/flutterbarcodescanner/FlutterBarcodeScannerPlugin.java
+++ android/src/main/java/com/amolg/flutterbarcodescanner/FlutterBarcodeScannerPlugin.java
@@ -146,24 +146,28 @@ public class FlutterBarcodeScannerPlugin implements MethodCallHandler, ActivityR
      */
     @Override
     public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RC_BARCODE_CAPTURE) {
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                if (data != null) {
-                    try {
-                        Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
-                        String barcodeResult = barcode.rawValue;
-                        pendingResult.success(barcodeResult);
-                    } catch (Exception e) {
+        synchronized (pendingResult){
+            if(pendingResult != null){
+                if (requestCode == RC_BARCODE_CAPTURE) {
+                    if (resultCode == CommonStatusCodes.SUCCESS) {
+                        if (data != null) {
+                            try {
+                                Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                                String barcodeResult = barcode.rawValue;
+                                pendingResult.success(barcodeResult);
+                            } catch (Exception e) {
+                                pendingResult.success("-1");
+                            }
+                        } else {
+                            pendingResult.success("-1");
+                        }
+                        pendingResult = null;
+                        arguments = null;
+                        return true;
+                    } else {
                         pendingResult.success("-1");
                     }
-                } else {
-                    pendingResult.success("-1");
                 }
-                pendingResult = null;
-                arguments = null;
-                return true;
-            } else {
-                pendingResult.success("-1");
             }
         }
         return false;
